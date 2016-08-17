2e91ab275938ffb0284ba49c69bd7cbfc26fef69
nikita <nikita@laptop.example.org>
2016-08-17 22:13:31 +0600

    Added cache support. Reconfigured serializer.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -22,6 +22,7 @@ class AppKernel extends Kernel
             new \Nelmio\ApiDocBundle\NelmioApiDocBundle(),
             new \Bazinga\Bundle\HateoasBundle\BazingaHateoasBundle(),
             new DoubleGis\TestBundle\DoubleGisTestBundle(),
+            new \FOS\HttpCacheBundle\FOSHttpCacheBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
